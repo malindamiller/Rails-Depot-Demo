43eaaf4e3caf85365b1e69cb00605396a55275a8
malindamiller <malindamiller2016@gmail.com>
2017-03-08 10:38:04 -0500

    intial commit depot app.

---
+++ db/migrate/20170308150858_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.string :text
+      t.string :image_url
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
