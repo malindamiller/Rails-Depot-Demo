686911ebd03eb0a75b89a2c2562c622ced26cd5d
malindamiller <malindamiller2016@gmail.com>
2017-03-09 09:22:21 -0500

    quantities shown in cart, and number of times accessed store.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = increment_count
+  end
+  def increment_count
+    if session[:count].nil?
+      session[:count] = 0
+    else
+      session[:count]+=1
+    end
   end
 end
