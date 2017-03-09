1311b07b96d4e6a043a21e93c7a20dc2459e7a11
malindamiller <malindamiller2016@gmail.com>
2017-03-09 10:33:05 -0500

    add total price.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309141116) do
+ActiveRecord::Schema.define(version: 20170309142325) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
